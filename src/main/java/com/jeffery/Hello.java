56e3338abf9e307c27e5f262798df1785e0a7181
Jeffery <chen.a905420@gmail.com>
2022-01-06 16:41:25 +0800

    Update!

--- src/main/java/com/jeffery/Hello.java
+++ src/main/java/com/jeffery/Hello.java
@@ -6,5 +6,17 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+
+        int age = 19;
+        Integer age2 = 19;
+        char c = 'A';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Jeffery";
+
     }
 }
--- src/main/kotlin/com/jeffery/Hello.kt
+++ src/main/kotlin/com/jeffery/Hello.kt
@@ -3,8 +3,17 @@ package com.jeffery
 fun main() {
 //    println("Hello Kotlin")
     Human().hello()
-    val p = Human()
-    p.hello()
+    val h = Human()
+    h.hello()
+
+    var age : Int = 109
+    age = 20;
+    var weight = 66.5f
+    val name = "Jeffery"
+    var name1 : String
+    name1 = "Jeffery2"
+
+
 }
 
 class Human {
